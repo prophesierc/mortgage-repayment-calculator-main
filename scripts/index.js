2f88e14922cfbdac29033d51a7b9472924044014
ProphesierC <bwknowles98@gmail.com>
2024-07-25 23:06:36 -0400

    WIP (Input formatting)

--- scripts/index.js
+++ scripts/index.js
@@ -11,16 +11,14 @@ class Calculator
         this.clear();
         this.radioSpanSelector();
         this.errorValidation();
+        this.toLocalStringInputFormatter();
     }
 
     clear()
-    {
-        const clearButton = document.querySelector('#clear');
-        const allInputs = document.querySelectorAll('input');
-        
-        clearButton.onclick = () =>
+    {        
+        document.querySelector('#clear').onclick = () =>
         {            
-            Array.from(allInputs).forEach((x) => 
+            Array.from(document.querySelectorAll('input')).forEach((x) => 
             {
                 x.checked = false;
                 x.value = null;    
@@ -42,7 +40,7 @@ class Calculator
         })   
     }
 
-    errorValidation()
+    errorValidation() // wip
     {
         const submitButton = document.querySelector('.submit');
         const allInputs = document.querySelectorAll('input');
@@ -56,6 +54,23 @@ class Calculator
                     console.log(x.value)
                 };
             })                               
+        }
+    }
+
+    toLocalStringInputFormatter() //wip
+    {
+        const input = document.querySelectorAll('input') // add all htmlcollections to function
+        input.onblur = () =>
+        {
+            const numericValue = input.value.replace(/[^\d]/g, '');
+            if (numericValue  !== '')
+            {
+                input.value = parseInt(numericValue, 10).toLocaleString();
+            }
+            else
+            {
+                input.value = ''
+            }
         }
     }
 
